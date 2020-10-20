cab84fb6d8990abcd93566b267fe9973f1381bcf
Thalita <Thalitarafaela16@gmail.com>
2020-10-20 19:14:48 -0300

    Adicionando Activity de redefinição de senha temporária

--- AndroidBoletimOnline/app/src/main/java/com/example/AndroidBoletimOnline/MainActivity.java
+++ AndroidBoletimOnline/app/src/main/java/com/example/AndroidBoletimOnline/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.AndroidBoletimOnline;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,6 +19,8 @@ import android.widget.TextView;
 import com.example.AndroidBoletimOnline.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     EditText User, Pwd;
     Button btnLogin;
@@ -43,56 +47,57 @@ public class MainActivity extends AppCompatActivity {
 
         User.requestFocus();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
-            @Override
-            public void onClick(View v) {
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
+                @Override
+                public void onClick(View v) {
 
-                String usuario = User.getText().toString(); //Obter usúario e senha dos objetos
-                String senha = Pwd.getText().toString();
+                    String usuario = User.getText().toString(); //Obter usúario e senha dos objetos
+                    String senha = Pwd.getText().toString();
 
-                if (usuario.isEmpty() || senha.isEmpty()) {//Verificar se estão vazio
-                    UsarMetodos.alert("Não deixe em branco.",
-                            getApplicationContext());
+                    if (usuario.isEmpty() || senha.isEmpty()) {//Verificar se estão vazio
+                        UsarMetodos.alert("Não deixe em branco.",
+                                getApplicationContext());
 
-                } else if (UsarMetodos.login(usuario, senha) == false) {//Passa usúario e senha
-                    UsarMetodos.alert("Usuário ou senha inválidos.",
-                            getApplicationContext());
-                    User.setText("");
-                    Pwd.setText("");
-                    User.requestFocus();
+                    } else if (UsarMetodos.login(usuario, senha) == false) {//Passa usúario e senha
+                        UsarMetodos.alert("Usuário ou senha inválidos.",
+                                getApplicationContext());
+                        User.setText("");
+                        Pwd.setText("");
+                        User.requestFocus();
 
-                } else if (binding.rbAluno.isChecked()) {
-                    Intent intent = new Intent(getApplicationContext(), AlunoActivity.class);
-                    startActivity(intent);
+                    } else if (binding.rbAluno.isChecked()) {
+                        Intent intent = new Intent(getApplicationContext(), AlunoActivity.class);
+                        startActivity(intent);
 
-                } else if (binding.rbProfessor.isChecked()) {
-                         Intent intent = new Intent(getApplicationContext(), ProfessorActivity.class);
-                         startActivity(intent);
-                     }
-                 }
+                    } else if (binding.rbProfessor.isChecked()) {
+                        Intent intent = new Intent(getApplicationContext(),RedefinirSenhaTempActivity.class);
+                        startActivity(intent);
+                    }
+                }
 
                 //} else {
-                    //Intent intent = new Intent(getApplicationContext(), AlunoActivity.class);
-                    //intent.putExtra("chave", usuario);
-                    //startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(), AlunoActivity.class);
+                //intent.putExtra("chave", usuario);
+                //startActivity(intent);
                 //}
-            //}
+                //}
 
-        });
-        btnRedefinirSenha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),TelaRedefinirSenha.class);
-                startActivity( intent );
-            }
-        });
+            });
+            btnRedefinirSenha.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), TelaRedefinirSenha.class);
+                    startActivity(intent);
+                }
+            });
 
+
+        }
+        @Override
+        protected void onDestroy () {
+            super.onDestroy();
+            binding = null;
+        }
     }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        binding = null;
-    }
 
-}
---
+++ AndroidBoletimOnline/app/src/main/java/com/example/AndroidBoletimOnline/RedefinirSenhaTempActivity.java
@@ -0,0 +1,69 @@
+package com.example.AndroidBoletimOnline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RedefinirSenhaTempActivity extends AppCompatActivity {
+
+   /* SharedPreferences sharedpreferences;
+    boolean splashScreen;
+
+    */
+   
+    TextView tvTextoRS;
+    EditText edtConfirSenhaTemp, edtSenhaTemp;
+    Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redefinir_senha_temp);
+
+        tvTextoRS = findViewById(R.id.tvTextoRS);
+        edtSenhaTemp = findViewById(R.id.edtSenhaTemp);
+        edtConfirSenhaTemp = findViewById(R.id.edtConfirSenhaTmp);
+        btnOk = findViewById(R.id.btnOk);
+
+        /*sharedpreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        splashScreen = sharedpreferences.getBoolean("splashScreen", false);
+        if (splashScreen == true) {
+            Intent mainIntent = new Intent(getApplicationContext(), ProfessorActivity.class);
+            startActivity(mainIntent);
+        } else {
+            setContentView(R.layout.activity_redefinir_senha_temp);
+        }
+
+         */
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String senhaT = edtSenhaTemp.getText().toString();
+                String confirS = edtConfirSenhaTemp.getText().toString();
+
+                if (senhaT.isEmpty() || confirS.isEmpty()) {//Verificar se estão vazios
+                    UsarMetodos.alert("Não deixe em branco.",
+                            getApplicationContext());
+                    edtSenhaTemp.setText("");
+                    edtConfirSenhaTemp.setText("");
+                    edtSenhaTemp.requestFocus();
+
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), ProfessorActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+
+        });
+    }
+}
